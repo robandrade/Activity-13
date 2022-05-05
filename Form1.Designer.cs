c3bfa792574cb12fb361d4fcd6ae9552d907ac4e
robandrade <103383943+robandrade@users.noreply.github.com>
2022-05-05 17:04:50 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button0_0 = new System.Windows.Forms.Button();
+            this.button0_1 = new System.Windows.Forms.Button();
+            this.button0_2 = new System.Windows.Forms.Button();
+            this.button1_0 = new System.Windows.Forms.Button();
+            this.button1_1 = new System.Windows.Forms.Button();
+            this.button1_2 = new System.Windows.Forms.Button();
+            this.button2_0 = new System.Windows.Forms.Button();
+            this.button2_1 = new System.Windows.Forms.Button();
+            this.button2_2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button0_0
+            // 
+            this.button0_0.Location = new System.Drawing.Point(101, 38);
+            this.button0_0.Name = "button0_0";
+            this.button0_0.Size = new System.Drawing.Size(75, 67);
+            this.button0_0.TabIndex = 0;
+            this.button0_0.UseVisualStyleBackColor = true;
+            this.button0_0.Click += new System.EventHandler(this.button0_0_Click);
+            // 
+            // button0_1
+            // 
+            this.button0_1.Location = new System.Drawing.Point(182, 38);
+            this.button0_1.Name = "button0_1";
+            this.button0_1.Size = new System.Drawing.Size(75, 67);
+            this.button0_1.TabIndex = 1;
+            this.button0_1.UseVisualStyleBackColor = true;
+            this.button0_1.Click += new System.EventHandler(this.button0_1_Click);
+            // 
+            // button0_2
+            // 
+            this.button0_2.Location = new System.Drawing.Point(263, 38);
+            this.button0_2.Name = "button0_2";
+            this.button0_2.Size = new System.Drawing.Size(75, 67);
+            this.button0_2.TabIndex = 2;
+            this.button0_2.UseVisualStyleBackColor = true;
+            this.button0_2.Click += new System.EventHandler(this.button0_2_Click);
+            // 
+            // button1_0
+            // 
+            this.button1_0.Location = new System.Drawing.Point(101, 111);
+            this.button1_0.Name = "button1_0";
+            this.button1_0.Size = new System.Drawing.Size(75, 67);
+            this.button1_0.TabIndex = 3;
+            this.button1_0.UseVisualStyleBackColor = true;
+            this.button1_0.Click += new System.EventHandler(this.button1_0_Click);
+            // 
+            // button1_1
+            // 
+            this.button1_1.Location = new System.Drawing.Point(182, 111);
+            this.button1_1.Name = "button1_1";
+            this.button1_1.Size = new System.Drawing.Size(75, 67);
+            this.button1_1.TabIndex = 4;
+            this.button1_1.UseVisualStyleBackColor = true;
+            this.button1_1.Click += new System.EventHandler(this.button1_1_Click);
+            // 
+            // button1_2
+            // 
+            this.button1_2.Location = new System.Drawing.Point(263, 111);
+            this.button1_2.Name = "button1_2";
+            this.button1_2.Size = new System.Drawing.Size(75, 67);
+            this.button1_2.TabIndex = 5;
+            this.button1_2.UseVisualStyleBackColor = true;
+            this.button1_2.Click += new System.EventHandler(this.button1_2_Click);
+            // 
+            // button2_0
+            // 
+            this.button2_0.Location = new System.Drawing.Point(101, 184);
+            this.button2_0.Name = "button2_0";
+            this.button2_0.Size = new System.Drawing.Size(75, 67);
+            this.button2_0.TabIndex = 6;
+            this.button2_0.UseVisualStyleBackColor = true;
+            this.button2_0.Click += new System.EventHandler(this.button2_0_Click);
+            // 
+            // button2_1
+            // 
+            this.button2_1.Location = new System.Drawing.Point(182, 184);
+            this.button2_1.Name = "button2_1";
+            this.button2_1.Size = new System.Drawing.Size(75, 67);
+            this.button2_1.TabIndex = 7;
+            this.button2_1.UseVisualStyleBackColor = true;
+            this.button2_1.Click += new System.EventHandler(this.button2_1_Click);
+            // 
+            // button2_2
+            // 
+            this.button2_2.Location = new System.Drawing.Point(263, 184);
+            this.button2_2.Name = "button2_2";
+            this.button2_2.Size = new System.Drawing.Size(75, 67);
+            this.button2_2.TabIndex = 8;
+            this.button2_2.UseVisualStyleBackColor = true;
+            this.button2_2.Click += new System.EventHandler(this.button2_2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 358);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2_2);
+            this.Controls.Add(this.button2_1);
+            this.Controls.Add(this.button2_0);
+            this.Controls.Add(this.button1_2);
+            this.Controls.Add(this.button1_1);
+            this.Controls.Add(this.button1_0);
+            this.Controls.Add(this.button0_2);
+            this.Controls.Add(this.button0_1);
+            this.Controls.Add(this.button0_0);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button0_0;
+        private System.Windows.Forms.Button button0_1;
+        private System.Windows.Forms.Button button0_2;
+        private System.Windows.Forms.Button button1_0;
+        private System.Windows.Forms.Button button1_1;
+        private System.Windows.Forms.Button button1_2;
+        private System.Windows.Forms.Button button2_0;
+        private System.Windows.Forms.Button button2_1;
+        private System.Windows.Forms.Button button2_2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_13
+{
+    public partial class Form1 : Form
+    {
+
+        private bool turnX = true;
+        private int[,] matrix = new int[3, 3];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button0_0_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button0_0.Text = "X";
+                matrix[0, 0] = 1;
+            }
+            else
+            {
+                button0_0.Text = "O";
+                matrix[0, 0] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button0_0.Enabled = false;
+        }
+
+        private void button0_1_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button0_1.Text = "X";
+                matrix[0, 1] = 1;
+            }
+            else
+            {
+                button0_1.Text = "O";
+                matrix[0, 1] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button0_1.Enabled = false;
+        }
+
+        private void button0_2_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button0_2.Text = "X";
+                matrix[0, 2] = 1;
+            }
+            else
+            {
+                button0_2.Text = "O";
+                matrix[0, 2] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button0_2.Enabled = false;
+        }
+
+        private void button1_0_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button1_0.Text = "X";
+                matrix[1, 0] = 1;
+            }
+            else
+            {
+                button1_0.Text = "O";
+                matrix[1, 0] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button1_0.Enabled = false;
+        }
+
+        private void button1_1_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button1_1.Text = "X";
+                matrix[1, 1] = 1;
+            }
+            else
+            {
+                button1_1.Text = "O";
+                matrix[1, 1] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button1_1.Enabled = false;
+        }
+
+        private void button1_2_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button1_2.Text = "X";
+                matrix[1, 2] = 1;
+            }
+            else
+            {
+                button1_2.Text = "O";
+                matrix[1, 2] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button1_2.Enabled = false;
+        }
+
+        private void button2_0_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button2_0.Text = "X";
+                matrix[2, 0] = 1;
+            }
+            else
+            {
+                button2_0.Text = "O";
+                matrix[2, 0] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button2_0.Enabled = false;
+        }
+
+        private void button2_1_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button2_1.Text = "X";
+                matrix[2, 1] = 1;
+            }
+            else
+            {
+                button2_1.Text = "O";
+                matrix[2, 1] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button2_1.Enabled = false;
+        }
+
+        private void button2_2_Click(object sender, EventArgs e)
+        {
+            if (turnX)
+            {
+                button2_2.Text = "X";
+                matrix[2, 2] = 1;
+            }
+            else
+            {
+                button2_2.Text = "O";
+                matrix[2, 2] = 2;
+            }
+            turnX = !turnX;
+            checkWinner();
+            button2_2.Enabled = false;
+        }
+
+        private void checkWinner()
+        {
+            // Check rows
+            if (matrix[0, 0] == matrix[0, 1] && matrix[0, 0] == matrix[0, 2])
+            {
+                if (matrix[0, 0] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[0, 0] == 2)
+                    label1.Text = "Player O is the winner";
+            }
+            else if (matrix[1, 0] == matrix[1, 1] && matrix[1, 0] == matrix[1, 2])
+            {
+                if (matrix[1, 0] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[1, 0] == 2)
+                    label1.Text = "Player O is the winner";
+            }
+            else if (matrix[2, 0] == matrix[2, 1] && matrix[2, 0] == matrix[2, 2])
+            {
+                if (matrix[2, 0] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[2, 0] == 2)
+                    label1.Text = "Player O is the winner";
+            }
+            // Check cols
+            if (matrix[0, 0] == matrix[1, 0] && matrix[0, 0] == matrix[2, 0])
+            {
+                if (matrix[0, 0] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[0, 0] == 2)
+                    label1.Text = "Player O is the winner";
+            }
+            else if (matrix[0, 1] == matrix[1, 1] && matrix[0, 1] == matrix[2, 1])
+            {
+                if (matrix[0, 1] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[0, 1] == 2)
+                    label1.Text = "Player O is the winner";
+            }
+            else if (matrix[0, 2] == matrix[1, 2] && matrix[0, 2] == matrix[2, 2])
+            {
+                if (matrix[0, 2] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[0, 2] == 1)
+                    label1.Text = "Player O is the winner";
+            }
+
+            // Diagonal
+            if (matrix[0, 0] == matrix[1, 1] && matrix[0, 0] == matrix[2, 2])
+            {
+                if (matrix[0, 0] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[0, 0] == 1)
+                    label1.Text = "Player O is the winner";
+            }
+            else if (matrix[0, 2] == matrix[1, 1] && matrix[0, 2] == matrix[2, 0])
+            {
+                if (matrix[0, 2] == 1)
+                    label1.Text = "Player X is the winner";
+                else if (matrix[0, 2] == 1)
+                    label1.Text = "Player O is the winner";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    matrix[i, j] = 0;
+        }
+    }
+}
